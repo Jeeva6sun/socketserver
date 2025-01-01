7187786f32f0d87e1a9c5cc5364d59e3e90bd271
jeeva somasundaram <103640102+jeevassomasundaram@users.noreply.github.com>
2025-01-01 13:00:53 +0530

    added welcome message

--- socket.go
+++ socket.go
@@ -2,10 +2,24 @@ package socketserver
 
 import "fmt"
 
+type userDetail struct {
+	id    int
+	name  string
+	email string
+}
+
 func Hello(name string) string {
 	return fmt.Sprintf("Hello mr , %s!", name)
 }
 
 func username(name string) string {
 	return fmt.Sprintf(name)
+}
+
+func useremail(name string) string {
+	return fmt.Sprintf(name + "@email.com")
+}
+
+func welcomemessage() string {
+	return fmt.Sprintf("welcome user")
 }
